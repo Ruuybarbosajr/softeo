2ffce8532ea729353bfec4e7b26990e134d24461
ruuybarbosajr <ruuybarbosajr@icloud.com>
2022-08-31 01:01:46 -0300

    refactory middleware appError and add actions on github

---
+++ src/database/Repository/index.ts
@@ -0,0 +1,5 @@
+import admin from './admin.repository';
+
+export default {
+  admin
+};
---
+++ src/middlewares/handleError.ts
@@ -0,0 +1,11 @@
+import { NextFunction, Request, Response } from 'express';
+import IAppError from '../interfaces/IAppError';
+import AppError from '../utils/appError';
+
+export default (error: Error | IAppError, _req: Request, res: Response, _next: NextFunction) => {
+  if (error instanceof AppError) {
+    return res.status(error.statusCode).json({ message: error.message });
+  }
+  console.log(error.message);
+  return res.status(500).json({ message: 'Internal server error '});
+};
