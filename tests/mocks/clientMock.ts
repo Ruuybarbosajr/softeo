dd06f55c075fac2a7870d8f6317ce676f46d4fb1
ruuybarbosajr <ruuybarbosajr@icloud.com>
2022-08-31 23:36:38 -0300

    test: add tests router update

--- tests/mocks/clientMock.ts
+++ tests/mocks/clientMock.ts
@@ -14,6 +14,13 @@ export const clientMock: Client = {
   tel: '2199157171'
 }
 
+export const clientUpdatedMock: Client = {
+  id: '197c7ac9-1054-44c1-909b-725a0fc14454',
+  name: 'Ju Ju bileu',
+  email: 'judiscleisson@gmail.com',
+  tel: '21999194372'
+}
+
 export const allClientMock: Client[] = [
   {
     id: '197c7ac9-1054-44c1-909b-725a0fc14454',
