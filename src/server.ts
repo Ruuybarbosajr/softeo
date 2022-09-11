04ec5e1974cb7cbbf0cca1a159cfbf8f2ecbc7d5
ruuybarbosajr <ruuybarbosajr@icloud.com>
2022-09-11 12:57:01 -0300

    mudança na port

--- src/server.ts
+++ src/server.ts
@@ -1,3 +1,3 @@
 import app from './api/app';
-
-app.listen(3001, () => console.log('server is running in port 3001'));
+const PORT = process.env.PORT || 3000; 
+app.listen(PORT, () => console.log(`server is running in port ${PORT}`));
