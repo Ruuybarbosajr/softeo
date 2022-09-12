b2cf18e12e2c135432eb85b997757e5b58c5d9cf
ruuybarbosajr <ruuybarbosajr@icloud.com>
2022-09-12 14:45:59 -0300

    add correção rota

--- src/api/app.ts
+++ src/api/app.ts
@@ -8,11 +8,13 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+
 app.use('/login', routes.login);
 app.use('/client', routes.client);
 app.use('/service', routes.service);
 app.use('/service-provided', routes.serviceProvided);
 
+app.use('/', (_req, res) => res.status(200).end('OK'));
 app.use(middlewares.handleError);
 
 export default app;
